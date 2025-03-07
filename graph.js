2a31ca4fc62ce4b1ffa3e1a3f15833745c00e56c
Rachel Curry <rachelcxd@gmail.com>
2025-03-07 12:44:25 -0800

    some more text added, graph not figured out

--- graph.js
+++ graph.js
@@ -2,8 +2,18 @@
 // Rachel Curry
 // March 7th, 2025
 
+const xValues = [50,60,70,80,90,100,110,120,130,140,150];
+const yValues = [7,8,8,9,9,9,10,11,14,14,15];
+
 const bigOGraph = new Chart("bigOGraph", {
     type: "line",
     data: {
-    }
+        labels: xValues,
+        datasets: [{
+          fill: false,
+          pointRadius: 1,
+          borderColor: "rgba(255,0,0,0.5)",
+          data: yValues
+        }]
+      }
 });
